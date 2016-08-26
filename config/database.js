ddb972320fe360f025553cc35c9bf64ec600f8ef
snowshine09 <greenwood_sonia@yahoo.com>
2016-08-26 15:33:14 -0400

    delete unrelated or not good to show to others

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // 'url': 'mongodb://localhost:27017/weare'
-    'url': 'mongodb://communityuser:4gVdtnBmTZ5zbKgE@localhost:27017/weare?authSource=admin'
+    'url': 'mongodb://localhost:27017/weare'
+    // 'url': 'mongodb://communityuser:4gVdtnBmTZ5zbKgE@localhost:27017/weare?authSource=admin'
 };
